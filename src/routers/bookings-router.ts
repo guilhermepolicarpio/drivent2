c3fb6f8d1938587b2f308e2a05c0f8798ab5511e
Guilherme <guilhermepolicarpio@gmail.com>
2022-12-04 17:56:22 -0300

    feat: update booking route added

--- src/routers/bookings-router.ts
+++ src/routers/bookings-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken, validateBody } from "@/middlewares";
-import { getBookingUser,postBooking } from "@/controllers/booking-controler"; 
+import { getBookingUser,postBooking, updateBooking } from "@/controllers/booking-controler"; 
 
 const bookingsRouter = Router();
 
@@ -8,5 +8,6 @@ bookingsRouter
   .all("/*", authenticateToken)
   .get("/", getBookingUser)
   .post("/", postBooking)
+  .put("/", updateBooking)
 
 export { bookingsRouter };
